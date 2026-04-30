d71f41b50a79f53e4a9c631e299e8ff3b49fc48e
waheddevs <abduvohid_gulomov@mail.ru>
2026-05-01 07:58:23 +0900

    thinking about fullstack

---
+++ notice.js
@@ -0,0 +1,12 @@
+/*
+
+Fullstack - Restaurant
+
+Frontend Development:
+    - BSSR (Backend server side rendering)(DTE/JTE/EJS)
+    - SPA (ReactJS/Angular/Veu)
+API REQUEST:
+    - Design     > Traditional API | Rest API | GraphQL API
+    - Method     > GET | POST
+    - Structure  > header | body
+*/
