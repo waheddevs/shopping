d87ed13809a28a854c1d45c4056a9eca2a0be8b7
waheddevs <abduvohid_gulomov@mail.ru>
2026-05-01 08:13:09 +0900

    feat: building simple backend server nodejs

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 // NodeJS  EXPRESS NESTJS
 // PYTHON  DJANGO FastAPI
+
+const express = require('express')
+const app = express()
+
+// Traditional API
+app.get('/', (req, res) => {
+  res.send('<h1>Hello World!</h1>')
+})
+
+// Rest API
+app.get('/rest', (req, res) => {
+  res.json({name: 'ARNOLD', age: 25, group: 'MIT 40'})
+})
+
+app.listen(3000, () => {
+  console.log(`Backend server is running on port 3000`)
+})
